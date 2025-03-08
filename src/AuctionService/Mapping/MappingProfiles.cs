9a77a7748541492a6f8100eb668d03625b4085be
Dimitrios Oikonomou <dimioiko96@hotmail.com>
2025-03-08 15:31:47 +0200

    add new models, add mapping, configure automapper for microservice

---
+++ src/AuctionService/Mapping/MappingProfiles.cs
@@ -0,0 +1,17 @@
+﻿using AuctionService.Entities;
+using AuctionService.Models;
+using AutoMapper;
+
+namespace AuctionService.Mapping
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Auction, AuctionModel>().IncludeMembers(x => x.Item);
+            CreateMap<Item, AuctionModel>();
+            CreateMap<CreateAuctionModel, Auction>().ForMember(d => d.Item, o => o.MapFrom(s => s));
+            CreateMap<CreateAuctionModel, Item>();
+        }
+    }
+}
