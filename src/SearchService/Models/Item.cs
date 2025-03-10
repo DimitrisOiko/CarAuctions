42de2d0e681a2db3b864b99a98ba4a361331ea44
DimitrisOiko <106746923+DimitrisOiko@users.noreply.github.com>
2025-03-10 21:36:33 +0200

    Feature/search service controller code (#5)
    * create search webapi, enrich docker-compose file, create DbInitializer
    
    * create search service controller and functionality
    
    ---------
    
    Co-authored-by: Dimitrios Oikonomou <dimioiko96@hotmail.com>

--- src/SearchService/Models/Item.cs
+++ src/SearchService/Models/Item.cs
@@ -6,7 +6,7 @@ namespace SearchService.Models
     {
         public int ReservePrice { get; set; }
         public required string Seller { get; set; }
-        public string? Winner { get; set; }
+        public string Winner { get; set; }
         public int? SoldAmount { get; set; }
         public int? CurrentHighBid { get; set; }
         public DateTime CreatedAt { get; set; }
