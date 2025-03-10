1a5e8f7aa85bbd9f44acda92d1c6f6101ec0a9bc
Dimitrios Oikonomou <dimioiko96@hotmail.com>
2025-03-10 12:53:39 +0200

    add CRUD apis, return IActionResult

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -3,9 +3,10 @@ using AuctionService.Entities;
 using AuctionService.Models;
 using AutoMapper;
 using MassTransit;
+using MassTransit.Transports;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
 
 namespace AuctionService.Controllers
 {
@@ -14,20 +15,81 @@ namespace AuctionService.Controllers
     public class AuctionsController(AuctionDbContext _context, IMapper _mapper) : ControllerBase
     {
         [HttpGet]
-        public async Task<ActionResult<List<AuctionModel>>> GetAllAuctions()
+        public async Task<IActionResult> GetAllAuctions()
         {
             List<Auction> auctions = await _context.Auctions.Include(x => x.Item).OrderBy(x => x.Item.Make).ToListAsync();
-            return _mapper.Map<List<AuctionModel>>(auctions);
+            return Ok(_mapper.Map<List<AuctionModel>>(auctions));
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<AuctionModel>> GetAuctionById(Guid id)
+        public async Task<IActionResult> GetAuctionById(Guid id)
         {
             var auction = await _context.Auctions.Include(x => x.Item).FirstOrDefaultAsync(x => x.Id == id);
 
             if (auction == null) return NotFound();
 
-            return _mapper.Map<AuctionModel>(auction);
+            return Ok(_mapper.Map<AuctionModel>(auction));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAuction(CreateAuctionModel auctionDto)
+        {
+            var auction = _mapper.Map<Auction>(auctionDto);
+
+            // after we have identity framework
+            //auction.Seller = User.Identity.Name;
+
+            _context.Add(auction);
+
+            var newAuction = _mapper.Map<AuctionModel>(auction);
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (!result) return BadRequest("Could not save changes to the DB");
+
+            return CreatedAtAction(nameof(GetAuctionById), new { auction.Id }, newAuction);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAuction(Guid id, UpdateAuctionModel updateAuctionDto)
+        {
+            var auction = await _context.Auctions.Include(x => x.Item).FirstOrDefaultAsync(x => x.Id == id);
+
+            if (auction == null) return NotFound();
+
+            // after we have identity framework
+            //if (auction.Seller != User.Identity?.Name) return Forbid();
+
+            auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
+            auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
+            auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
+            auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
+            auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result) return Ok();
+
+            return BadRequest("Problem saving changes");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAuction(Guid id)
+        {
+            var auction = await _context.Auctions.FindAsync(id);
+
+            if (auction == null) return NotFound();
+
+            // after we have identity framework
+            //if (auction.Seller != User.Identity?.Name) return Forbid();
+
+            _context.Auctions.Remove(auction);
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (!result) return BadRequest("Could not update DB");
+
+            return Ok();
         }
     }
 }
