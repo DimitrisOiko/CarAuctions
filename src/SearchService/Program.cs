893b00e8f51b3f80ed26444baf599af513586739
Dimitrios Oikonomou <dimioiko96@hotmail.com>
2025-03-10 22:22:08 +0200

    add retry policy on polling the auction service

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,5 +1,8 @@
+using Polly.Extensions.Http;
+using Polly;
 using SearchService.Data;
 using SearchService.Services;
+using System.Net;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-builder.Services.AddHttpClient<AuctionSvcHttpClient>();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -27,3 +30,9 @@ catch (Exception)
 { }
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
