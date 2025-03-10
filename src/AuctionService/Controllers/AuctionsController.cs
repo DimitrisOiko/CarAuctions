c3cc8bb0f3ed5e835ab975c867bd8322f09241c9
DimitrisOiko <106746923+DimitrisOiko@users.noreply.github.com>
2025-03-10 22:29:27 +0200

    Feature/auction service (#6)
    * fix migrations
    
    * add seed data to auctions table
    
    * add new models, add mapping, configure automapper for microservice
    
    * add new controller with 2 new endpoints
    
    * add CRUD apis, return IActionResult
    
    * fix git ignore
    
    * refactor GetAllAuctions
    
    ---------
    
    Co-authored-by: Dimitrios Oikonomou <dimioiko96@hotmail.com>

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -2,6 +2,7 @@
 using AuctionService.Entities;
 using AuctionService.Models;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using MassTransit;
 using MassTransit.Transports;
 using Microsoft.AspNetCore.Authorization;
@@ -15,14 +16,47 @@ namespace AuctionService.Controllers
     public class AuctionsController(AuctionDbContext _context, IMapper _mapper) : ControllerBase
     {
         [HttpGet]
-        public async Task<IActionResult> GetAllAuctions()
+        public async Task<IActionResult> GetAllAuctions(string date)
         {
-            List<Auction> auctions = await _context.Auctions.Include(x => x.Item).OrderBy(x => x.Item.Make).ToListAsync();
-            return Ok(_mapper.Map<List<AuctionModel>>(auctions));
+            var query = _context.Auctions.OrderBy(x => x.Item.Make).AsQueryable();
+
+            if (!string.IsNullOrEmpty(date))
+                query = query.Where(x => x.UpdatedAt.CompareTo(DateTime.Parse(date).ToUniversalTime()) > 0);
+
+            return Ok(await query.ProjectTo<AuctionModel>(_mapper.ConfigurationProvider).ToListAsync());
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetAuctionById(Guid id)
+        {
+            var auction = await _context.Auctions.Include(x => x.Item).FirstOrDefaultAsync(x => x.Id == id);
+
+            if (auction == null) return NotFound();
+
+            return Ok(_mapper.Map<AuctionModel>(auction));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAuction(CreateAuctionModel auctionDto)
+        {
+            var auction = _mapper.Map<Auction>(auctionDto);
+
+            // after we have identity framework
+            //auction.Seller = User.Identity.Name;
+
+            _context.Add(auction);
+
+            var newAuction = _mapper.Map<AuctionModel>(auction);
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (!result) return BadRequest("Could not save changes to the DB");
+
+            return CreatedAtAction(nameof(GetAuctionById), new { auction.Id }, newAuction);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAuction(Guid id, UpdateAuctionModel updateAuctionDto)
         {
             var auction = await _context.Auctions.Include(x => x.Item).FirstOrDefaultAsync(x => x.Id == id);
 
