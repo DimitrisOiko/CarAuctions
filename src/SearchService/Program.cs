58ffc2728f7c37037454f555d63d6b3b96db8a3d
DimitrisOiko <106746923+DimitrisOiko@users.noreply.github.com>
2025-03-10 22:30:33 +0200

    Feature/search service (#7)
    * create search webapi, enrich docker-compose file, create DbInitializer
    
    * create search service controller and functionality
    
    * create http communication for services
    
    * add retry policy on polling the auction service
    
    * minnor fix to start app without initializing db
    
    ---------
    
    Co-authored-by: Dimitrios Oikonomou <dimioiko96@hotmail.com>

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,4 +1,8 @@
+using Polly.Extensions.Http;
+using Polly;
 using SearchService.Data;
+using SearchService.Services;
+using System.Net;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,7 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -17,11 +22,18 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
+
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    await DbInitializer.InitDb(app);
-}
-catch (Exception)
-{ }
+    await Policy.Handle<TimeoutException>()
+        .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(10))
+        .ExecuteAndCaptureAsync(async () => await DbInitializer.InitDb(app));
+});
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
