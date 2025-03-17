a2deadda090f6a880f91c492d956b9bf42fb703d
Dimitrios Oikonomou <dimioiko96@hotmail.com>
2025-03-17 17:25:23 +0200

    add rabbit mq for service communication

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -3,6 +3,7 @@ using AuctionService.Entities;
 using AuctionService.Models;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Contracts;
 using MassTransit;
 using MassTransit.Transports;
 using Microsoft.AspNetCore.Authorization;
@@ -13,7 +14,7 @@ namespace AuctionService.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class AuctionsController(AuctionDbContext _context, IMapper _mapper) : ControllerBase
+    public class AuctionsController(AuctionDbContext _context, IMapper _mapper, IPublishEndpoint publishEndpoint) : ControllerBase
     {
         [HttpGet]
         public async Task<IActionResult> GetAllAuctions(string date)
@@ -47,37 +48,9 @@ namespace AuctionService.Controllers
             _context.Add(auction);
 
             var newAuction = _mapper.Map<AuctionModel>(auction);
-
+            await publishEndpoint.Publish(_mapper.Map<AuctionCreated>(newAuction));
             var result = await _context.SaveChangesAsync() > 0;
 
-            if (!result) return BadRequest("Could not save changes to the DB");
-
-            return CreatedAtAction(nameof(GetAuctionById), new { auction.Id }, newAuction);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateAuction(Guid id, UpdateAuctionModel updateAuctionDto)
-        {
-            var auction = await _context.Auctions.Include(x => x.Item).FirstOrDefaultAsync(x => x.Id == id);
-
-            if (auction == null) return NotFound();
-
-            return Ok(_mapper.Map<AuctionModel>(auction));
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> CreateAuction(CreateAuctionModel auctionDto)
-        {
-            var auction = _mapper.Map<Auction>(auctionDto);
-
-            // after we have identity framework
-            //auction.Seller = User.Identity.Name;
-
-            _context.Add(auction);
-
-            var newAuction = _mapper.Map<AuctionModel>(auction);
-
-            var result = await _context.SaveChangesAsync() > 0;
 
             if (!result) return BadRequest("Could not save changes to the DB");
 
---
+++ src/SearchService/Consumers/AuctionCreatedConsumer.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+using static MassTransit.ValidationResultExtensions;
+
+namespace SearchService.Consumers
+{
+    public class AuctionCreatedConsumer(IMapper mapper) : IConsumer<AuctionCreated>
+    {
+        public async Task Consume(ConsumeContext<AuctionCreated> context)
+        {
+            var item = mapper.Map<Item>(context.Message);
+
+            await item.SaveAsync();
+        }
+    }
+}
