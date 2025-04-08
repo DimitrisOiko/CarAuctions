b5a11554e77839b24ca91a359b72861a80aed7d5
DimitrisOiko <106746923+DimitrisOiko@users.noreply.github.com>
2025-04-08 14:25:31 +0300

    include GatewayService, new consumers (#15)

---
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using AuctionService.Data;
+using AuctionService.Entities;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers;
+
+public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+{
+    private readonly AuctionDbContext _dbContext;
+
+    public AuctionFinishedConsumer(AuctionDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    { 
+        var auction = await _dbContext.Auctions.FindAsync(Guid.Parse(context.Message.AuctionId))
+            ?? throw new MessageException(typeof(AuctionFinished), "Cannot retrieve this auction");
+
+        if (context.Message.ItemSold)
+        {
+            auction.Winner = context.Message.Winner;
+            auction.SoldAmount = context.Message.Amount;
+        }
+
+        auction.Status = auction.SoldAmount > auction.ReservePrice
+            ? Status.Finished : Status.ReserveNotMet;
+
+        await _dbContext.SaveChangesAsync();
+    }
+}
--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -21,7 +21,6 @@ public static class Config
     public static IEnumerable<Client> Clients =>
         new Client[]
         {
-            // interactive client using code flow + pkce
             new Client
             {
                 ClientId = "postman",
@@ -30,6 +29,18 @@ public static class Config
                 RedirectUris = { "https://www.getpostman.com/oauth2/callback" },
                 ClientSecrets = { new Secret("Secret".Sha256()) },
                 AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+            },
+            new Client
+            {
+                ClientId = "nextApp",
+                ClientName = "nextApp",
+                AllowedScopes = { "openid", "profile", "auctionApp" },
+                RedirectUris = { "http://localhost:3000/api/auth/callback/id-server" },
+                ClientSecrets = { new Secret("Secret".Sha256()) },
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                // This is false because the app is not native, in a react appliction this would be true and AllowedGrantTypes whould be code
+                RequirePkce = false,
+                AllowOfflineAccess = true
             },
         };
 }
